572c2e160e828322c984616f24859210cd519266
“vernfleming” <10568@holbertonstudents.com>
2025-04-14 21:17:20 +1000

    question 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of ints
+ * @array: array of ints
+ * @size: number of elements in array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_index;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -21,10 +21,9 @@ struct listint_s *next;
 
 void print_array(const int *array, size_t size);
 void print_list(const struct listint_s *list);
-
-
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 
 #endif
