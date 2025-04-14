50d4486a79d60fb4585d2d095dd995c19fe126e8
“vernfleming” <10568@holbertonstudents.com>
2025-04-14 21:23:36 +1000

    fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -36,3 +36,24 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 	{
 		if (array[j] <= pivot)
 		{
+i++;
+			if (i != j)
+			{
+				int temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+
+	if (i + 1 != high)
+	{
+		int temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+
+	return (i + 1);
+}
